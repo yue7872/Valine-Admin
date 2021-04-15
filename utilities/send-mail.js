fb7e0bdfd82582345c69c6c5678d9fbeaf037afd
HCLonely <26346802+HCLonely@users.noreply.github.com>
2021-04-15 10:02:50 +0800

    修复QQ提醒失败的bug

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -99,7 +99,7 @@ ${text}
   if (process.env.QMSG_KEY != null) {
     /*
     if (process.env.QQ_SHAKE != null) {
-      axios.get(`https://qmsg.zendee.cn:443/send/${process.env.QMSG_KEY}.html?msg=${encodeURIComponent('[CQ:shake]')}`)
+      axios.get(`https://qmsg.zendee.cn:443/send/${process.env.QMSG_KEY}?msg=${encodeURIComponent('[CQ:shake]')}`)
         .then(function (response) {
           if (response.status === 200 && response.data.success === true) {
             console.log('已发送QQ戳一戳')
